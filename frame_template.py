0cf810bcbeab470ac97b06973831eb346918e76d
Adamanski817 <adamdutoit8@gmail.com>
2025-11-11 14:53:32 +0000

    I tried and failed to get this code to work

--- frame_template.py
+++ frame_template.py
@@ -46,6 +46,7 @@ class flashcard_module(tk.Frame):
 def flashcard_mode(self):
     print ("haaaaiiii vro im lkey a twink")
     central= flashcard_module(self)
+    # ask sir if it is more complicated to make the modules run concurrently or just change the frame
 
         
 

939ad5fd11d51bc6cfe4e3704daec90e5a4912ee
Adamanski817 <adamdutoit8@gmail.com>
2025-11-11 13:35:14 +0000

    Changed the button function

--- frame_template.py
+++ frame_template.py
@@ -20,13 +20,13 @@ class central(tk.Frame):
         self.to_do_list_label = tk.Label(self,text="to do core",borderwidth=3,relief="groove")
         self.to_do_list_label.grid(row=0,column = 3)
 
-        self.flashcard_button= tk.Button(self,text= "open flashcard", command= lambda: flashcard_module(self))
+        self.flashcard_button= tk.Button(self,text= "open flashcard", command= lambda: flashcard_mode(self))
         self.flashcard_button.grid (row=1,column=0)
-        self.notepad_button= tk.Button(self,text= "open notepad", command= lambda: flashcard_module(self))
+        self.notepad_button= tk.Button(self,text= "open notepad", command= lambda: flashcard_mode(self))
         self.notepad_button.grid (row=1,column=1)
-        self.calender_button= tk.Button(self,text= "open calender", command= lambda: flashcard_module(self))
+        self.calender_button= tk.Button(self,text= "open calender", command= lambda: flashcard_mode(self))
         self.calender_button.grid (row=1,column=2)
-        self.to_do_list_button= tk.Button(self,text= "open to-do list", command= lambda: flashcard_module(self))
+        self.to_do_list_button= tk.Button(self,text= "open to-do list", command= lambda: flashcard_mode(self))
         self.to_do_list_button.grid (row=1,column=3)
 
 
@@ -43,7 +43,7 @@ class flashcard_module(tk.Frame):
 
 
 
-def flashcard_module(self):
+def flashcard_mode(self):
     print ("haaaaiiii vro im lkey a twink")
     central= flashcard_module(self)
 
